173bbdb946d3accb3588e2114825896eba4cb8b7
Eric Kwok <ericc.kwok@gmail.com>
2016-10-29 15:35:17 -0700

    create rails skeleton

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+entry: "./frontend/vivo.jsx",
+  
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', `react`]
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  }
+};
