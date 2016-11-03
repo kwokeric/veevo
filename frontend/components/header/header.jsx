80b348ea4c299f8c01242daace7053264e39e20f
Eric Kwok <ericc.kwok@gmail.com>
2016-11-02 22:28:49 -0700

    add bootstrap

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+
 import UserDisplay from './user_display';
 import LogInButton from './login_button';
 
 var Modal = require("react-modal");
 
+// class Header extends React.Component {
+
 const Header = ({ login, signup, currentUser, logout }) => {
+  // getInitialState: function() {
+  //   return({ modalOpen: false })
+  // }
+
   let sessionButton;
 
   if (currentUser) {
@@ -24,8 +31,15 @@ const Header = ({ login, signup, currentUser, logout }) => {
         alt="Veevo Logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Vevo_2016_Logo.svg/1000px-Vevo_2016_Logo.svg.png" />
       {sessionButton}
+
+
     </nav>
   );
 };
 
 export default Header;
+// <Modal
+//   isOpen={this.state.modalOpen}
+//   >
+//
+// </Modal>
