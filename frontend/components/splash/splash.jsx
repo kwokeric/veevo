7743c0829ae263c4400bd2d8a3b2295a76613d93
Eric Kwok <ericc.kwok@gmail.com>
2016-12-01 14:55:19 -0800

    Add alternating backgrounds on splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -11,6 +11,31 @@ class Splash extends React.Component {
     this.demo = this.demo.bind(this);
   }
 
+  componentDidMount() {
+    let splash = $(".splash");
+    let backgrounds = [
+      `url("http://res.cloudinary.com/kwokeric/image/upload/v1480630916/drake_eusmb3.gif")`,
+      `url("http://res.cloudinary.com/kwokeric/image/upload/v1480631437/coldplay_cjnxvj.gif")`,
+      `url("http://res.cloudinary.com/kwokeric/image/upload/v1480629473/flume_fxgefz.gif")`,
+      `url("http://res.cloudinary.com/kwokeric/image/upload/v1480628748/major_lazer_dj_snake_anufhr.gif")`,
+      `url("http://res.cloudinary.com/kwokeric/image/upload/v1480630310/kendrick_c3xohm.gif")`,
+      `url("http://res.cloudinary.com/kwokeric/image/upload/v1480629719/ok_go_dvb7ig.gif")`
+    ];
+    let current = 0;
+
+    function nextBackground() {
+      splash.css(
+        "background-image",
+        backgrounds[current = ++current % backgrounds.length]
+      );
+
+      setTimeout(nextBackground, 5500);
+    }
+
+    setTimeout(nextBackground, 5500);
+    splash.css("background-image", backgrounds[0]);
+  }
+
   handleDemo(e) {
     e.preventDefault();
     const guest = {username: "guest", password: "password"};
@@ -21,8 +46,8 @@ class Splash extends React.Component {
     e.preventDefault();
 
     $('html, body').animate({
-      scrollTop: $(".carousel-title").offset().top
-    }, 1000);
+      scrollTop: $(".carousel-title").offset().top - 10
+    }, 800);
   }
 
   headline () {
@@ -57,9 +82,11 @@ class Splash extends React.Component {
     return(
       <div className="index">
         <div className="splash">
+          <div className="splash-filter"></div>
+          
           {this.headline()}
           {this.demo()}
-          <i className="fa fa-chevron-down" aria-hidden="true" onCLick={this.handleScroll}></i>
+          <i className="fa fa-chevron-down" aria-hidden="true" onClick={this.handleScroll}></i>
         </div>
 
         <IndexCarousels videos={this.props.videos}/>
